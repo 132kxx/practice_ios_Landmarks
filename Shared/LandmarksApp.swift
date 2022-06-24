e3a275c23059a67b6dc086c250eeb16df2a019a5
kxx <132kxx@gmail.com>
2022-06-24 14:59:26 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by kxx: on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
